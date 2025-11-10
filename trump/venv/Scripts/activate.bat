15ec9c573563b23c66a8a451db5dfbd0c84fc3de
Lan Ngai <b00159127@mytudublin.ie>
2025-11-10 23:27:21 +0000

    Shoulder Surfing
    This section was written up regarding the exploit and the remediation effort in the source code. The source code changes were also made regarding that

--- trump/venv/Scripts/activate.bat
+++ trump/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lanng\OneDrive - Technological University Dublin\Y3_Sem1\Secure Programming\Assignment1\trump\venv
+set VIRTUAL_ENV=C:\Users\lanng\OneDrive - Technological University Dublin\Y3_Sem1\Secure Programming\SecureProgramming-Assignment1\trump\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
